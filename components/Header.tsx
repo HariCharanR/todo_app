c35159b7489c1f4cb17a4c79bfeef18152a88679
Hari Charan <hariRagu2831@gmail.com>
2023-12-29 19:35:55 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -58,10 +58,10 @@ function Header() {
 
             </div>
             <div className="flex items-center justify-center px-5 md:py-5 ">
-                <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl bg-white rounded-md'>
+                {/* <p className='flex items-center p-5 text-sm font-light pr-5 shadow-xl bg-white rounded-md'>
                     <UserCircleIcon className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${loading && "animate-spin"} `}  />
                     {suggestion && !loading ? suggestion : "GPT is summerising your todos "}
-                </p>
+                </p> */}
             </div>
 
         </header>
